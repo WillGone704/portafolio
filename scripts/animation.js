b234c8d84ed41b4f594158ec221f37603d9447dd
WillGone704 <wilgon725@protonmail.com>
2019-09-12 12:25:18 -0400

    animating the portafolio

---
+++ scripts/animation.js
@@ -0,0 +1,6 @@
+//Algoritmo para animar los hexagones
+//-----------------------------------------
+// 1. Mapear los hexagones: se debe verificar cuantos hexagones hay,
+//  la data debe ser guarda en un Array
+
+// 2.
